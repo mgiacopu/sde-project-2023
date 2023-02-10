d38a6e32be55e556c9dd153bf4878c5c2116288c
FedeC <24813315+turbostar190@users.noreply.github.com>
2023-02-10 18:21:13 +0100

    Data layer on top of routes

--- data-layers/index.js
+++ data-layers/index.js
@@ -6,30 +6,47 @@ const axios = require('axios');
  * Default configuration for axios web requests
  */
 axios.defaults.method = 'get';
-axios.defaults.headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/109.0'};
+axios.defaults.headers = { 
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/109.0', 
+};
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const { parseLonLat } = require('./middleware');
 
 /**
- * ROUTES
- */
+ * ADAPTER LAYER
+*/
+const routerAdapterLayer = express.Router();
+const { parseLonLat } = require('./middleware');
+
 const geocodings = require('./adapters/geocoding');
-app.use('/v1/geocoding/', geocodings);
+routerAdapterLayer.use('/v1/geocoding/', geocodings);
 const air_pollution = require('./adapters/air_pollution');
-app.use('/v1/air_pollution/', parseLonLat, air_pollution);
+routerAdapterLayer.use('/v1/air_pollution/', parseLonLat, air_pollution);
 const weather = require('./adapters/weather');
-app.use('/v1/weather/', parseLonLat, weather);
+routerAdapterLayer.use('/v1/weather/', parseLonLat, weather);
 const map = require('./adapters/map');
-app.use('/v1/map/', parseLonLat, map);
+routerAdapterLayer.use('/v1/map/', parseLonLat, map);
 const places = require('./adapters/places');
-app.use('/v1/places/', parseLonLat, places);
+routerAdapterLayer.use('/v1/places/', parseLonLat, places);
+
+/**
+ * USER DATABASE
+ */
+const routerDatabase = express.Router();
 
 const db_user = require('./db/user');
-app.use('/v1/db/user', db_user);
+routerDatabase.use('/v1/user', db_user);
+
+
+/**
+ * DATA LAYER
+ */
+app.use('/api/adapters/', routerAdapterLayer);
+app.use('/api/db/', routerDatabase);
+
 
 const httpServer = http.createServer(app);
 httpServer.listen(80, () => {
