526bc6dc77b974924dff52482900b4598c67217d
FedeC <24813315+turbostar190@users.noreply.github.com>
2023-02-13 11:03:38 +0100

    `weather/forecast`

--- data-layers/adapters/weather.js
+++ data-layers/adapters/weather.js
@@ -24,17 +24,61 @@ router.get('/current', function (req, res) {
             ...CONFIG.params,
             q: `${lat},${lon}`,
             alerts: "yes",
-            days: 1,
-            aqi: "yes",
+            // days: 1,
         }
     };
     axios(config)
         .then(response => {
-            res.status(200).json(response.data);
+            let ret = {
+                current: response.data.current,
+                alerts: response.data.alerts,
+            }
+            res.status(200).json(ret);
         })
         .catch(err => {
-            console.log(err.response)
-            res.status(500).json(err);
+            console.log(err)
+            res.status(500).json({error: err});
+        });
+});
+
+
+router.get('/forecast', function (req, res) { 
+    const lat = req.lat;
+    const lon = req.lon;
+    const dayQuery = req.query.day;
+    const dt = new Date(dayQuery);
+    if (!dayQuery || dt.toString() === "Invalid Date") {
+        res.status(400).json({ error: "Invalid day" });
+        return;
+    }
+    
+    const day = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
+    
+    let config = {
+        ...CONFIG,
+        params: {
+            ...CONFIG.params,
+            q: `${lat},${lon}`,
+            alerts: "yes",
+            dt: day,
+        }
+    };
+    axios(config)
+        .then(response => {
+            if (response.data.forecast.forecastday.length === 0) {
+                res.status(400).json({ error: "Forecast day must be within 14 days" });
+                return;
+            } else {
+                let ret = {
+                    [response.data.forecast.forecastday[0].date]: response.data.forecast.forecastday[0].day,
+                    alerts: response.data.alerts,
+                }
+                res.status(200).json(ret);
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: err});
         });
 });
 
